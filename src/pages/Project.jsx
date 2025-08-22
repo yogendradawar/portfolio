0a651c872bfce1d384093218637c271696a88ca4
Yogendra <yogendradawar6@gmail.com>
2025-08-22 09:43:02 +0530

    Update Initial

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,37 +1,43 @@
-
-import "./Project.css"
+import "./Project.css";
 import { forwardRef } from "react";
-export const Project=forwardRef((props,ref)=>{
-    return <>
-     <div className="projects" ref={ref}>
+export const Project = forwardRef((props, ref) => {
+  return (
+    <>
+      <div className="projects" ref={ref}>
         <h1>Projects</h1>
-        <p>Completed multiple projects, enhancing my development skills significantly.</p>
+        <p>
+          Completed multiple projects, enhancing my development skills
+          significantly.
+        </p>
         <div className="project-container">
+          <div className="project-card">
+            <img
+              src="https://i.pinimg.com/736x/bb/96/3d/bb963d1a494773393db00e169d3108fe.jpg"
+              alt=""
+            />
 
-        {/* <div className="project-card">
-                <img src="https://etutor.co/blog/wp-content/uploads/2023/03/online-exam-software.jpg" alt="" />
+            <div className="technologies">
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JavaScript</p>
+              <p>EJS</p>
+              <p>Node.js</p>
+              <p>Express.js</p>
+              <p>MongoDB</p>
+              <p>Cloudinary</p>
+              <p>Passport.js</p>
+              <p>Mapbox</p>
+            </div>
+            <p id="title">WanderLust - Travel Accommodation Platform</p>
+<p id="date">Mar 2024 - Jul 2024</p>
+<p id="description">Developed a full-stack travel accommodation platform similar to Airbnb with user authentication, property management, interactive maps, reviews, and bookings. Built with MERN stack, EJS templating, and Cloudinary image hosting.</p>
+<div className="links">
+    <a href="https://github.com/yogendradawar/WanderLust" target="_blank" id="code">View Code</a>
+    <a href="https://wanderlust-vv2u.onrender.com/" target="_blank" id="live">View Live</a>
+</div>
+          </div>
 
-                <div className="technologies">
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>Tailwind</p>
-                    <p>Java Script</p>
-                    <p>React.js</p>
-                    <p>Node.js</p>
-                    <p>Express.js</p>
-                    <p>Mongodb</p>
-                </div>
-                <p id="title">TestPro- Online Test Taking Platform</p>
-                <p id="date">Aug 2024 - Nov 2024</p>
-                <p id="description">Created a fully functional and responsive online test-taking platform using React.js, Tailwind CSS, and
-                a simple backend with Express.js.</p>
-                <div className="links">
-                    <a href="https://github.com/Surjeet1808/TestPro-Online-Test-Platform.git" target="_blank" id="code">View Code</a>
-                    <a href="https://test-pro-online.vercel.app/" target="_blank" id="live">View Live</a>
-                </div>
-            </div> */}
-
-           {/* <div className="project-card">
+          {/* <div className="project-card">
                 <img src="https://wedevs.com/_ipx/https://cdn.wedevs.com/uploads/2021/09/How-to-use-Dokan-Booking-Module.png?f=webp&q=90" alt="" />
 
                 <div className="technologies">
@@ -53,47 +59,76 @@ export const Project=forwardRef((props,ref)=>{
                 </div>
             </div> */}
 
+          <div className="project-card">
+            <img
+              src="https://mobisoftinfotech.com/resources/wp-content/uploads/2018/08/Banner.png"
+              alt=""
+            />
 
-            <div className="project-card">
-                <img src="https://www.dealsshutter.com/blog/wp-content/uploads/2020/03/Home-Services-Image-30-march-2020.png" alt="" />
-
-                <div className="technologies">
-                    <p>Mongodb</p>
-                    <p>Express.js</p>
-                    <p>React.js</p>
-                    <p>Node.js</p>
-                </div>
-                <p id="title">Service Space</p>
-                <p id="date">March 2024 - May 2024</p>
-                <p id="description">Service Space is a Platform offering a variety of services at home. Customers use platform to book services such as  cleaning, plumbing, carpentry, appliance repair, painting etc.</p>
-                <div className="links">
-                    <a href="https://github.com/yogendradawar/service-space/tree/main/frontend/frontend" target="_blank" id="code">View Code</a>
-                    <a href="https://service-space-zd79.vercel.app/" target="_blank" id="live">View Live</a>
-                </div>
+            <div className="technologies">
+              <p>Mongodb</p>
+              <p>Express.js</p>
+              <p>React.js</p>
+              <p>Node.js</p>
             </div>
-
-
-
-            <div className="project-card">
-                <img src="https://pmstudycircle.com/wp-content/uploads/2024/01/portfolio.png.webp" alt="" />
-
-                <div className="technologies">
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React.js</p>
-                </div>
-                <p id="title">Portfolio Website - Front-end Development</p>
-                <p id="date">Feb 2024 - March 2024</p>
-                <p id="description">Developed a responsive Portfolio Website using React.js, incorporating React hooks and fundamental frontend development principles.</p>
-                <div className="links">
-                    <a href="https://github.com/yogendradawar/portfolio" target="_blank" id="code">View Code</a>
-                    <a href="#" target="_blank" id="live">View Live</a>
-                </div>
+            <p id="title">Service Space</p>
+            <p id="date">March 2024 - May 2024</p>
+            <p id="description">
+              Service Space is a Platform offering a variety of services at
+              home. Customers use platform to book services such as cleaning,
+              plumbing, carpentry, appliance repair, painting etc.
+            </p>
+            <div className="links">
+              <a
+                href="https://github.com/yogendradawar/service-space/tree/main/frontend/frontend"
+                target="_blank"
+                id="code"
+              >
+                View Code
+              </a>
+              <a
+                href="https://service-space-zd79.vercel.app/"
+                target="_blank"
+                id="live"
+              >
+                View Live
+              </a>
             </div>
+          </div>
 
+          <div className="project-card">
+            <img
+              src="https://uploads.sitepoint.com/wp-content/uploads/2019/04/155530806117.jpg"
+              alt=""
+            />
 
+            <div className="technologies">
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>React.js</p>
+            </div>
+            <p id="title">Portfolio Website - Front-end Development</p>
+            <p id="date">Feb 2024 - March 2024</p>
+            <p id="description">
+              Developed a responsive Portfolio Website using React.js,
+              incorporating React hooks and fundamental frontend development
+              principles.
+            </p>
+            <div className="links">
+              <a
+                href="https://github.com/yogendradawar/portfolio"
+                target="_blank"
+                id="code"
+              >
+                View Code
+              </a>
+              <a href="#" target="_blank" id="live">
+                View Live
+              </a>
+            </div>
+          </div>
 
-            {/* <div className="project-card">
+          {/* <div className="project-card">
                 <img src="https://www.iqutest.com/wp-content/uploads/2023/07/Simon-Game.jpg" alt="" />
 
                 <div className="technologies">
@@ -110,9 +145,7 @@ export const Project=forwardRef((props,ref)=>{
                 </div>
             </div> */}
 
-
-
-            {/* <div className="project-card">
+          {/* <div className="project-card">
                 <img src="https://media.licdn.com/dms/image/D4D12AQFidZRD7yMEHQ/article-cover_image-shrink_720_1280/0/1691241743325?e=2147483647&v=beta&t=PyST8q7CDL5x_GIzslj4rTy8_WHkKIftMW6f6AjWOiQ" alt="" />
 
                 <div className="technologies">
@@ -128,30 +161,49 @@ export const Project=forwardRef((props,ref)=>{
                 </div>
             </div> */}
 
-              <div className="project-card">
-                <img src="https://media.licdn.com/dms/image/D4D12AQFidZRD7yMEHQ/article-cover_image-shrink_720_1280/0/1691241743325?e=2147483647&v=beta&t=PyST8q7CDL5x_GIzslj4rTy8_WHkKIftMW6f6AjWOiQ" alt="" />
+          <div className="project-card">
+            <img
+              src="https://aureatelabs.com/wp-content/uploads/2022/11/31-eCommerce-Website-Pages-List_-Must-Have-Pages-Elements-With-Examples.jpg"
+              alt=""
+            />
 
-                <div className="technologies">
-                    <p>HTM</p>
-                    <p>CSS</p>
-                    <p>Java Script</p>
-                </div>
-                <p id="title">E-commerce Website</p>
-                <p id="date">Oct 2023 - Nov 2023</p>
-                <p id="description">Designed and developed a fully functional e-commerce website frontend using HTML, CSS, and JavaScript, including homepage and product listing.<br />
-    Implemented a shopping cart feature with add, remove, and update item capabilities.<br />
-    Created a contact section that sends emails using EmailJS, allowing users to reach out easily.</p>
-                <div className="links">
-                    <a href="http://github.com/yogendradawar/e-commerce-cart-functionality." target="_blank" id="code">View Code</a>
-                    <a href="https://e-commerce-cart-functionality.netlify.app/" target="_blank" id="live">View Live</a>
-                </div>
+            <div className="technologies">
+              <p>HTM</p>
+              <p>CSS</p>
+              <p>Java Script</p>
             </div>
-
-
-
+            <p id="title">E-commerce Website</p>
+            <p id="date">Oct 2023 - Nov 2023</p>
+            <p id="description">
+              Designed and developed a fully functional e-commerce website
+              frontend using HTML, CSS, and JavaScript, including homepage and
+              product listing.
+              <br />
+              Implemented a shopping cart feature with add, remove, and update
+              item capabilities.
+              <br />
+              Created a contact section that sends emails using EmailJS,
+              allowing users to reach out easily.
+            </p>
+            <div className="links">
+              <a
+                href="http://github.com/yogendradawar/e-commerce-cart-functionality."
+                target="_blank"
+                id="code"
+              >
+                View Code
+              </a>
+              <a
+                href="https://e-commerce-cart-functionality.netlify.app/"
+                target="_blank"
+                id="live"
+              >
+                View Live
+              </a>
+            </div>
+          </div>
         </div>
-     </div>
-
+      </div>
     </>
-}
-);
+  );
+});
